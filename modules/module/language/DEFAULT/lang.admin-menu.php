a0b6d639ca1d215caa349a696a787a9e8bab03b5
George Seongho Kim <admin@kimsq.com>
2014-11-01 02:44:44 +0900

    Reset

--- modules/module/language/DEFAULT/lang.admin-menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-// menu
-$lang['module']['menu1'] = '등록정보';
-$lang['module']['menu2'] = '업데이트';
-$lang['module']['menu3'] = '익스텐션';
-?>
--- modules/module/language/english/lang.admin-menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-// menu
-$lang['module']['menu1'] = 'Settings'; //등록정보
-$lang['module']['menu2'] = 'Update'; //업데이트
-$lang['module']['menu3'] = 'Extension'; //익스텐션
-?>
