04e16f2686615a8bab4f1ed6d8dfd33678634e2b
kieregh <kiere@naver.com>
2015-04-12 10:01:11 +0900

    레이아웃 부트스트랩 테마제거/타이틀에 이미지 등록 관련 변수 수정

--- layouts/default/_var/_var.php
+++ layouts/default/_var/_var.php
@@ -1,6 +1,6 @@
 <?php
-$d['layout']['theme_kind'] = "default";
 $d['layout']['header_title'] = "My homepage";
+$d['layout']['header_file'] = "";
 $d['layout']['header_fixed'] = "true";
 $d['layout']['header_trans'] = "true";
 $d['layout']['header_search'] = "true";
