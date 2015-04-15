bd7a23c41e8c9df16bb6c3d0404cd569698c30e7
kieregh <kiere@naver.com>
2015-04-15 17:53:19 +0900

    관리자 회원삭제 기능 오류 수정
    https://github.com/kimsQ/rb/pull/90

--- modules/admin/action/a.admin_delete.php
+++ modules/admin/action/a.admin_delete.php
@@ -6,46 +6,47 @@ checkAdmin(0);
 foreach ($mbrmembers as $val)
 {
 	if ($my['uid'] == $val) continue;
-	$M=getDbData($table['s_mbrid'],'uid='.$val,'*');
+	$M1 = getDbData($table['s_mbrdata'],'memberuid='.$val,'*');
+	$M2 = getUidData($table['s_mbrid'],$val);
 	if($auth)
 	{
 		if ($auth == 'D')
 		{
-			getDbDelete($table['s_mbrid'],'uid='.$M['memberuid']);
-			getDbDelete($table['s_mbrdata'],'memberuid='.$M['memberuid']);
-			getDbDelete($table['s_mbrcomp'],'memberuid='.$M['memberuid']);
-			getDbDelete($table['s_paper'],'my_mbruid='.$M['memberuid']);
-			getDbDelete($table['s_point'],'my_mbruid='.$M['memberuid']);
-			getDbDelete($table['s_scrap'],'mbruid='.$M['memberuid']);
-			getDbDelete($table['s_simbol'],'mbruid='.$M['memberuid']);
-			getDbDelete($table['s_friend'],'my_mbruid='.$M['memberuid'].' or by_mbruid='.$M['memberuid']);
-			getDbUpdate($table['s_mbrlevel'],'num=num-1','uid='.$M['level']);
-			getDbUpdate($table['s_mbrgroup'],'num=num-1','uid='.$M['sosok']);
-			getDbDelete($table['s_mbrsns'],'memberuid='.$M['memberuid']);
+			getDbDelete($table['s_mbrid'],'uid='.$M1['memberuid']);
+			getDbDelete($table['s_mbrdata'],'memberuid='.$M1['memberuid']);
+			getDbDelete($table['s_mbrcomp'],'memberuid='.$M1['memberuid']);
+			getDbDelete($table['s_paper'],'my_mbruid='.$M1['memberuid']);
+			getDbDelete($table['s_point'],'my_mbruid='.$M1['memberuid']);
+			getDbDelete($table['s_scrap'],'mbruid='.$M1['memberuid']);
+			getDbDelete($table['s_simbol'],'mbruid='.$M1['memberuid']);
+			getDbDelete($table['s_friend'],'my_mbruid='.$M1['memberuid'].' or by_mbruid='.$M1['memberuid']);
+			getDbUpdate($table['s_mbrlevel'],'num=num-1','uid='.$M1['level']);
+			getDbUpdate($table['s_mbrgroup'],'num=num-1','uid='.$M1['sosok']);
+			getDbDelete($table['s_mbrsns'],'memberuid='.$M1['memberuid']);
 
-			if (is_file($g['path_var'].'avatar/'.$M['photo']))
+			if (is_file($g['path_var'].'avatar/'.$M1['photo']))
 			{
-				unlink($g['path_var'].'avatar/'.$M['photo']);
+				unlink($g['path_var'].'avatar/'.$M1['photo']);
 			}
-			if (is_file($g['path_var'].'avatar/180.'.$M['photo']))
+			if (is_file($g['path_var'].'avatar/180.'.$M1['photo']))
 			{
-				unlink($g['path_var'].'avatar/180.'.$M['photo']);
+				unlink($g['path_var'].'avatar/180.'.$M1['photo']);
 			}
-			$fp = fopen($g['path_tmp'].'out/'.$M['id'].'.txt','w');
+			$fp = fopen($g['path_tmp'].'out/'.$M2['id'].'.txt','w');
 			fwrite($fp,$date['totime']);
 			fclose($fp);
-			@chmod($g['path_tmp'].'out/'.$M['id'].'.txt',0707);
+			@chmod($g['path_tmp'].'out/'.$M2['id'].'.txt',0707);
 		}
 		else if ($auth == 'A')
 		{
-			getDbUpdate($table['s_mbrdata'],"admin=1,adm_view='[admin]'",'memberuid='.$M['uid']);
+			getDbUpdate($table['s_mbrdata'],"admin=1,adm_view='[admin]'",'memberuid='.$M2['uid']);
 		}
 		else {
-			getDbUpdate($table['s_mbrdata'],"auth='$auth'",'memberuid='.$M['uid']);
+			getDbUpdate($table['s_mbrdata'],"auth='$auth'",'memberuid='.$M2['uid']);
 		}
 	}
 	else {
-		getDbUpdate($table['s_mbrdata'],"admin=0,adm_view=''",'memberuid='.$M['uid']);
+		getDbUpdate($table['s_mbrdata'],"admin=0,adm_view=''",'memberuid='.$M2['uid']);
 	}
 }
 
