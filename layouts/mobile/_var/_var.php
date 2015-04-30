64ea33400268d632a8f2c68a4ec93a144a6ea3ab
gitaeks <break@kimsq.com>
2015-04-30 09:04:56 +0900

    모바일 기본 레이아웃 제거
    * boostrap 응용 레이아웃은 입문자들의 학습에 혼선을 줄수 있어 킴스큐 마켓을 통해서 제공하려는 계획으로 삭제 처리함
    * 모바일전용 ratchet기반 레이아웃 추가 예정임

--- layouts/mobile/_var/_var.php
+++
@@ -1,7 +0,0 @@
-<?php
-$d['layout']['header_title'] = "My homepage";
-$d['layout']['header_login'] = "true";
-$d['layout']['header_notify'] = "true";
-$d['layout']['front_slide'] = "";
-$d['layout']['front_people'] = "";
-?>
