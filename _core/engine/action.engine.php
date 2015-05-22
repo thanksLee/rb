e356027b4cafb33eea38fb6bf2c19ed23ef206dd
gitaeks <break@kimsq.com>
2015-05-22 11:18:10 +0900

    XSS 및 CSRF 공격차단 보안패치(3차)
    XSS(Cross-site scripting, 크로스 사이트 스크립팅) 나 CSRF(Cross Site Request
    Forgery, 크로스 사이트 요청 변조)공격을 관리자 옵션에 따라 차단할 수 있도록 했습니다.
    
    패치내용
    --------
    XSS(Cross-site scripting, 크로스 사이트 스크립팅) 및 CSRF(Cross Site Request
    Forgery, 크로스 사이트 요청 변조)공격에 대한 추가보완 패치입니다.

--- _core/engine/action.engine.php
+++ _core/engine/action.engine.php
@@ -7,6 +7,12 @@ foreach ($_filterSet as $_ft)
 	${$_ft} = strip_tags(${$_ft});
 }
 
+if ($_SERVER['HTTP_REFERER'] && strpos($_SERVER['HTTP_REFERER'],'m=bbs'))
+{
+	if ($m != 'bbs') exit;
+	
+}
+
 $g['act_module0'] = $g['dir_module'].$a.'.php';
 $g['act_module1'] = $g['dir_module'].'lang.'.$_HS['lang'].'/action/'.(strpos($a,'/')?str_replace('/','/a.',$a):'a.'.$a).'.php';
 $g['act_module2'] = $g['dir_module'].'action/a.'.$a.'.php';
