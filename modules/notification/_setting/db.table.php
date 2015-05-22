31230d46e1e6a9a809bc98de35930bb55707f0e9
gitaeks <break@kimsq.com>
2015-05-22 11:13:20 +0900

    Revert "알림센터 모듈추가"
    This reverts commit 61c905301d36e5c582c0b5e86af55c8ef23e91f3.

--- modules/notification/_setting/db.table.php
+++
@@ -1,4 +0,0 @@
-<?php
-$table[$module.'config'] = $DB['head'].'_'.$module.'_config'; //알림설정
-$table[$module.'data'] = $DB['head'].'_'.$module.'_data'; //알림데이터
-?>
--- modules/notification/var/var.php
+++
@@ -1,4 +0,0 @@
-<?php
-$d['notification']['cut_modules'] = "";
-$d['notification']['cut_members'] = "";
-?>
